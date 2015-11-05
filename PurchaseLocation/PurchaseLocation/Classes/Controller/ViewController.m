4680ee6eb4edfff0a73004a92887f88d30965bda
tangShunZhi <1046975221@qq.com>
2015-11-05 10:58:32 -0800

    1、实现自定义大头针显示的图片 和 显示的视图 2、显示大头针的视图需要  遵守 MKMapViewDelegate 协议，实现显示视图的方法

--- PurchaseLocation/PurchaseLocation/Classes/Controller/ViewController.m
+++ PurchaseLocation/PurchaseLocation/Classes/Controller/ViewController.m
@@ -8,7 +8,7 @@
 #import <MapKit/MapKit.h>
 
 #import "TSZAnnotation.h"
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 
@@ -22,6 +22,8 @@
     //添加 拖拽手势  ,实现 添加大头针的方法
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(addAnnotationInMapView:)];
     
+    //遵守协议
+    self.mapView.delegate = self;
     
     //添加手势给 mapView
     [self.mapView addGestureRecognizer:tap];
@@ -39,10 +41,50 @@
     //3、把点坐标转换成 经纬度
     anno.coordinate = [self.mapView convertPoint:point toCoordinateFromView:self.mapView];
     
+    //一个 ktv的图标
     anno.title = @"北京";
     anno.imageName = @"category_2";
     
     //4、添加大头针
     [self.mapView addAnnotation:anno];
 }
+
+#pragma mark: MKMapViewDelegate协议
+
+/**
+ *  每添加一个大头针 模型  就会调用的代理方法  返回一个大头针视图
+ *
+ *  @param mapView    mapView
+ *  @param annotation 大头针模型
+ */
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+    static NSString *ID = @"anno";
+    
+    //大头针视图
+    MKAnnotationView *view = [mapView dequeueReusableAnnotationViewWithIdentifier:ID];
+    
+    /**
+     * 判断 试图为空就创建一个
+     */
+    if (view == nil) {
+        view = [[MKAnnotationView alloc]initWithAnnotation:annotation reuseIdentifier:ID];
+        
+        view.canShowCallout = YES;
+        //
+        view.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeContactAdd];
+        
+        view.leftCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        
+    }
+    
+    TSZAnnotation *anno = (TSZAnnotation *)annotation;
+    
+    view.image = [UIImage imageNamed:anno.imageName];
+    
+    return view;
+    
+}
+
+
 @end
